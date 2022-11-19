f1ee20ea85748301db4de9453ed639ee27f8d88b
Jared Flores <90533617+JaredFG@users.noreply.github.com>
2022-11-19 04:30:25 -0600

    update in player controller

--- Exsanea/Assets/Scripts/test.cs
+++ Exsanea/Assets/Scripts/test.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
 
 public class test : MonoBehaviour
 {
@@ -8,21 +9,35 @@ public class test : MonoBehaviour
     public GameObject actualPlane;
     public GameObject newPlane;
 
+    public GameObject prefab;
+    public Transform spawnPoint;
+    public GameObject gameObjectPlayer;
+    [SerializeField] private CinemachineVirtualCamera Nextcam;
+
+    private void Start()
+    {
+        gameObjectPlayer = GameObject.FindGameObjectWithTag("Player");
+    }
     private void OnTriggerStay2D(Collider2D other)
     {
         if (other.tag == "Player")
         {
             if (other.gameObject.GetComponent<PlayerController>().isInteracting == true)
             {
-                StartCoroutine(interaction());
+                changeView();
             }
         }
     }
-    IEnumerator interaction()
+    public void changeView()
     {
-        newPlane.SetActive(true);
-        yield return new WaitForSeconds(2);
-        actualPlane.SetActive(false);
-    }
+        //playeble director
+        if (CameraSwitcher.ActiveCamera != Nextcam) CameraSwitcher.SwitchCamera(Nextcam);
+        Destroy(gameObjectPlayer);
+        Instantiate(prefab, spawnPoint.position, spawnPoint.rotation);
 
+        actualPlane.SetActive(false);
+        newPlane.SetActive(true);
+
+
+    }
 }
